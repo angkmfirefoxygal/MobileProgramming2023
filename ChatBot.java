d7c2d94ad381745fb68acf275d27ec8eca4926e8
MOON WON <junexmun@gmail.com>
2023-11-15 00:12:36 +0900

    Create ChatBot.java

---
+++ ChatBot.java
@@ -0,0 +1,18 @@
+package com.example.team2app_toolbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ChatBot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_bot);
+    }
+
+
+}
