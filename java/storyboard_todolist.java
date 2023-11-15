b0e32dd8bbede1d09f0fc7046bcbae00c85dcfd6
MOON WON <junexmun@gmail.com>
2023-11-15 22:43:26 +0900

    Create storyboard_todolist.java

---
+++ java/storyboard_todolist.java
@@ -0,0 +1,12 @@
+public class storyboard_todolist extends Fragment{
+  @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_storyboard_todolist, container, false);
+
+        return rootView;
+    }
+}
+
+
