7c93bf127d9bff21d82d13bc5ca5f40ac63dc78d
MOON WON <junexmun@gmail.com>
2023-11-15 22:46:20 +0900

    Update storyboard_todolist.java

--- java/storyboard_todolist.java
+++ java/storyboard_todolist.java
@@ -1,3 +1,4 @@
+// (new) - (fragments) - (blank fragments ) 생성 후 이 부분만 변경해주면 됩니다. 
 public class storyboard_todolist extends Fragment{
   @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
